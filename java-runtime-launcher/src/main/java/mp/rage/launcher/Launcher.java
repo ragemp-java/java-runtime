060a28147da00ea99061d3aa5d996e5ce828cff8
Fabian Jungwirth <fabian@jungwirth.ovh>
2017-11-07 17:42:26 +0100

    #11 Implement launcher startup system

--- java-runtime-launcher/src/main/java/mp/rage/launcher/Launcher.java
+++ java-runtime-launcher/src/main/java/mp/rage/launcher/Launcher.java
@@ -11,15 +11,31 @@
 package mp.rage.launcher;
 
 import lombok.extern.slf4j.Slf4j;
+import mp.rage.api.exception.JNIExecutionException;
+
+import java.io.File;
 
 @Slf4j
 public class Launcher {
 
     private Launcher() {}
 
-    public static void main() {
-        log.info("Initializing RageJava Launcher");
-        log.info("Loading JNI Library");
-        System.load("C:\\Users\\Fabian Jungwirth\\CLionProjects\\ragejava\\build\\plugins\\RageJava.dll");
+    /**
+     * @param operatingSystem 0 for linux and 1 for windows
+     */
+    public static void main(int operatingSystem) throws JNIExecutionException {
+        log.info("Initializing RageJava launcher");
+        String fileEnding;
+        if(operatingSystem == 0) {
+            fileEnding = ".so";
+            log.info("Using Linux binary");
+        } else if (operatingSystem  == 1) {
+            fileEnding = ".dll";
+            log.info("Using Windows binary");
+        } else {
+            throw new JNIExecutionException("invalid operating system parameter: " + operatingSystem);
+        }
+        File binaryPath = new File("plugins/RageJava" + fileEnding);
+        System.load(binaryPath.getAbsolutePath());
     }
 }
